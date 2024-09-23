640bd42e409a5de4d3bb398e84b5585977cb5964
An3loN <vladrorokin@mail.ru>
2024-09-23 18:21:00 +0300

    got rid of unused func

--- ansible_quickstart/app/web/main.py
+++ ansible_quickstart/app/web/main.py
@@ -23,31 +23,6 @@ encode_times = []
 frame_times = []
 start_time = None
 
-def generate():
-    global start_time
-    while True:
-        if not start_time:
-            start_time = time()
-        if time() - start_time >= 10:
-            print('average cpu load: ', sum(cpu_load_stamps)/len(cpu_load_stamps))
-            print('average image encode time: ', sum(encode_times)/len(encode_times))
-            print('average frame time: ', sum(frame_times)/len(frame_times))
-            cpu_load_stamps.clear()
-            encode_times.clear()
-            frame_times.clear()
-            start_time = time()
-        for image in images:
-            frame_start = time()
-            encode_start = time()
-            ret, jpeg = cv2.imencode('.jpg', image)
-            encode_times.append(time() - encode_start)
-            cpu_load_stamps.append(cpu_percent())
-            frame = jpeg.tobytes()
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-            frame_times.append(time() - frame_start)
-
 @app.route("/")
 def hello_world():
     return send_from_directory('static', 'index.html')
